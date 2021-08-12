cf0aa4cd624cb7f095595717e50e1a964222e6c2
danny <47242861+contrafactum@users.noreply.github.com>
2021-08-12 15:24:15 +0100

    Added calculator function

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+function updateTextInput(val) {
+    document.getElementById('textInput').value=val; 
+  }
+
 function compute()
 {
+    // storing the values as inputted by the user
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    t = document.getElementById("year").value;
+
+    // Simple interest formula:
+    // A = p * r * t, where
+    // A is the final amount, p is the principal value
+    // r is the interest rate in %
+    // and t is the number of years
+
+    // calculating the interest from the data above
+    a = p * r * t;
+
+
+    alert("rig");
 }
         
