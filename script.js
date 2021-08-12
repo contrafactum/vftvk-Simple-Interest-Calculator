d52ae9acb0dd35a46f7bb60dd60524ee7d1edefa
danny <47242861+contrafactum@users.noreply.github.com>
2021-08-12 15:57:00 +0100

    Added the count functionality, Added footer.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function updateTextInput(val) {
-    document.getElementById("range").value=val; 
-}
+    document.getElementById('textInput').value=val + "%"; 
+  }
 
 function compute()
 {
     // storing the values as inputted by the user
     p = document.getElementById("principal").value;
     r = document.getElementById("rate").value;
-    t = document.getElementById("year").value;
+    t = document.getElementById("years").value;
 
     // Simple interest formula:
     // A = p * r * t, where
@@ -18,7 +18,20 @@ function compute()
     // calculating the interest from the data above
     a = p * r * t;
 
+    // getting the current year
+    var d = new Date();
+    var current_year = d.getFullYear();
+    
+    // creating the 'compound message'
+    message = "If you deposit " + p + ", at an interest rate of " + r + "%." + "You will receive an amount of " + a + "in the year " + (current_year+t);
 
-    alert("rig");
+    // writing the compound information message to the respective element
+
+ /*    node = document.getElementById("result");
+    newnode = document.createElement('p');
+    newnode.appendChild(document.createTextNode(message));
+    node.appendChild(newnode); */
+
+    document.getElementById('result').innerHTML = message;
 }
         
