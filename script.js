f5e14927f4bbc07939352cc1fc90b7f8dea79907
danny <47242861+contrafactum@users.noreply.github.com>
2021-08-13 10:13:22 +0100

    Finished the project.

--- script.js
+++ script.js
@@ -4,10 +4,26 @@ function updateTextInput(val) {
 
 function compute()
 {
+
     // storing the values as inputted by the user
     p = document.getElementById("principal").value;
     r = document.getElementById("rate").value;
     t = document.getElementById("years").value;
+
+    // input validation
+    if (p==0) {
+      document.getElementById("amount_right_space").innerHTML = "Please provide the principal value!";
+      return;
+    } else {
+      document.getElementById("amount_right_space").innerHTML = "";
+    }
+
+    if (t==0) {
+      document.getElementById("years_right_space").innerHTML = "Please provide the years value!";
+      return;
+    } else {
+      document.getElementById("years_right_space").innerHTML = "";
+    }
 
     // Simple interest formula:
     // A = p * r * t, where
@@ -18,20 +34,17 @@ function compute()
     // calculating the interest from the data above
     a = p * r * t;
 
-    // getting the current year
+    // getting the current year and calculating the new year
     var d = new Date();
-    var current_year = d.getFullYear();
+    var current_year = Number(d.getFullYear());
+    var new_year = Number(current_year) + Number(t);
     
     // creating the 'compound message'
-    message = "If you deposit " + p + ", at an interest rate of " + r + "%." + "You will receive an amount of " + a + "in the year " + (current_year+t);
+    message = "If you deposit <mark>" + p + "</mark>,<br> at an interest rate of <mark>" + r + "%</mark>.<br>" + "You will receive an amount of <mark>" + a.toFixed(2) + "</mark>,<br>in the year <mark>" + new_year + "</mark>";
 
     // writing the compound information message to the respective element
-
- /*    node = document.getElementById("result");
-    newnode = document.createElement('p');
-    newnode.appendChild(document.createTextNode(message));
-    node.appendChild(newnode); */
-
-    document.getElementById('result').innerHTML = message;
+    document.body.style.height = "40vh";
+    document.getElementById('message').style.paddingTop = "10px";
+    document.getElementById('message').innerHTML = message;
 }
         
